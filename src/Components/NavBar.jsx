aa217371671400798da46a129c67a702f530c28c
Meghan Carter <msc2010@yahoo.com>
2023-09-27 12:37:42 -0500

    navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function NavBar() {
+    return(
+        <nav>
+            <h2>Fitness</h2>
+            <h2>Nutrition</h2>
+        </nav>
+    )
+}
+
+export default NavBar;
