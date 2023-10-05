c5b66d7e3ffcc81bcfa6de6a2a415309908898a1
Meghan Carter <msc2010@yahoo.com>
2023-10-05 18:34:23 -0500

    increment function

--- src/Components/Nutrition.jsx
+++ src/Components/Nutrition.jsx
@@ -1,44 +1,19 @@
 import React from "react";
-import {useState} from "react";
+import Increment from "./Increment";
 
 function Nutrition() {
-    
-    const [count, setCount]=useState(0);
-
-    function increment(){
-        setCount(function (prevCount) {
-                return (prevCount += 1)
-        })
-    }
-
-    function decrement(){
-        setCount(function (prevCount) {
-            if (prevCount>0) {
-                return (prevCount -= 1)
-            } else {
-                return (prevCount = 0)
-            }
-        })
-    }
-
+ 
     return(
         <div id="nutrition">
             
             <h2>FRUITS</h2>
-
-            <h1>{count}</h1>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
-
+            <Increment/>
+            
             <h2>VEGETABLES</h2>
-            <h1>{count}</h1>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
+            <Increment/>
 
             <h2>WATER</h2> 
-            <h1>{count}</h1>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
+            <Increment/>
         </div>
 
 
