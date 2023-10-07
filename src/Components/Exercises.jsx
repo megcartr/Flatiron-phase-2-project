bc841c618a289eda4cf66fc507a8c10f51bf7dbc
Meghan Carter <msc2010@yahoo.com>
2023-10-07 17:08:19 -0500

    increment in excercise

--- src/Components/Exercises.jsx
+++ src/Components/Exercises.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Increment from "./Increment";
 
 function Exercises({type, image}) {
     return(
         <div>
             <h2>{type}</h2>
+            <Increment/> <h2>minutes</h2>
             <img src={image}></img>
         </div>
     )
