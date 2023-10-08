fbe94862f146256ee312ff9fe5fc25fa7114e4a5
Meghan Carter <msc2010@yahoo.com>
2023-10-08 09:12:18 -0500

    post request

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,10 +5,18 @@ function Form() {
    const [minutes, setMinutes] = useState("");
    const handleSubmit = (e)=>{e.preventDefault()}
 
+   fetch("saveContact/", {
+    method: "POST",
+    body: JSON.stringify(minutes),
+    headers: {
+        "Content-Type": "application/json"
+    },
+})
+
     return(
     <form onSubmit={handleSubmit}>
         <input onChange= {(e) => setMinutes(e.target.value)}
-        value={minutes}/>
+        value={minutes} placeholder="            duration"/>
         <button type= "submit">submit</button>
     </form>  
 )}
